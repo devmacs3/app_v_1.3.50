ba3e0de9bd1fa2a440f5fd7ebf00e3fb77c56a40
emerson <emerson.sousasilva@saude.ba.gov.br>
2020-03-06 09:42:09 -0300

    app_1.3.50

---
+++ www/js/services/global.service.js
@@ -0,0 +1,29 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('GlobalService', GlobalService);
+
+    GlobalService.$inject = [];
+
+    /**
+     * Sets global parameters
+     */
+    function GlobalService() {
+        var service = {
+            initialState: "app.home",
+            base64Prefix: "data:image/jpeg;base64,",
+            //serverBaseUri: "http://desenv.macs.sesab.ba.gov.br",
+            //serverBaseUri: "http://homologa.mapadasaude.sesab.ba.gov.br",
+            serverBaseUri: "http://10.17.178.176:9000",
+            authorizationData: {},
+            formTemplates: [],
+            eventsPath: "sync/events/image?img="
+        };
+
+        return service;
+    }
+
+})();
